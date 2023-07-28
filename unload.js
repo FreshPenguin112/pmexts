6b72d43187fdfba2581b9a7a81cc3a8c8bb19de4
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-07-27 19:07:35 -0500

    fvgrfefrv

---
+++ unload.js
@@ -0,0 +1,27 @@
+(function(Scratch) {
+    'use strict';
+  
+    if (!Scratch.extensions.unsandboxed) {
+      throw new Error('This Extension Must Run Unsandboxed');
+    }
+    var vm = Scratch.vm;
+    window.onbeforeunload = function(){vm.runtime.startHats("unload_onunload")};
+  
+    class ext {
+      getInfo() {
+        return {
+          id: 'unload',
+          name: 'Unload',
+          blocks: [
+            {
+              opcode: 'onunload',
+              blockType: Scratch.BlockType.HAT,
+              text: 'when tab closed',
+              isEdgeActivated: false
+            }
+          ]
+        };
+      }}
+    Scratch.extensions.register(new ext());
+  })(Scratch);
+  
